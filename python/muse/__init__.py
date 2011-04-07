ae1fae9f13cc7678b7b5d02c8636c3dc16bffecd
Joseph Anderson <josephlloydanderson@mac.com>
2011-04-07 11:22:40 +0100

    Muse __init__ updated:  use PSL if audiolab unavailable.
    __init__ updated with error catching. Muse uses the PSL sndfile module if audiolab is unavailable.
    
    Also: warning about deprecation of the 'old' sndfiles (mult file I/O using audiolab).
    
    At some point, __init__ import of sndfiles should be removed.
    
    Signed-off-by: Joseph Anderson <josephlloydanderson@mac.com>

--- python/muse/__init__.py
+++ python/muse/__init__.py
@@ -4,41 +4,87 @@ Muse
 
 This is the start of the Muse project, a Music V type language.
 
-Built on pyaudiolab.
 """
 
-# Likely need to sort out all the various imports.
-# Of particular interest, name collisions
-# between numpy array functions and math / python functions
+# ***** Need to sort out...
+#
+#       Namespace collisions. At the moment all numpy is imported into muse,
+#       which can cause some problems: particularly with numpy, muse, math,
+#       and python functions.
 
-# __all__ var, list muse modules here
-# __all__ = ["muse", "filters", "decoders", "encoders", "transforms"]
 
-# import numpy and pyaudiolab into muse namespace
-from numpy import *
-# from pyaudiolab import *
-from scikits.audiolab import *
+# ***** Nice starting messages
+print 'Starting up Muse!\n'
+
+try:                                            # try numpy...
+    print 'Importing numpy...'
+    from numpy import *
+    print '                         ... success!'
+
+except ImportError:
+    print '     ... oh, no! \nNumpy not found! \Muse depends on numpy!'
+
+else:
+    print 'Importing muse...'
+    from muse import *                          # import main muse...
+    from filters    import *
+    from decoders   import *
+    from encoders   import *
+    from transforms import *
+    from generators import *
+    from nonlinear  import *
+    from analysis   import *
+    print '                         ... success!'
+
+    
+    try:
+        print 'Importing audiolab...'           # try audiolab...
+        from scikits.audiolab import *
+        print '                         ... success!'
+
+        print 'Importing sndfiles...'
+        from sndfiles import *        # old soundfiles module to be deprecated
+        print '                         ... success'
+
+        print '                             WARNING: Sndfiles uses the old ' +\
+              'audiolab API, \n' +\
+              '                                      and is soon to be ' +\
+              'deprecated.'
+
+        try:                                    # try samplerate...
+            print 'Importing samplerate...'
+            from scikits.samplerate import resample as src
+            print '                         ... success!'
+
+        except ImportError:
+            print '                         ... samplerate unavailable.'
+
+    except ImportError:
+        print '                         ... audiolab unavailable!'
+        print '                         ... using muse sndfile (PSL), instead.'
+        from sndfile import *        # the new PSL aifc/wave interface
+
+        print 'Samplerate is unavailable...'     
+
+    print "\nWelcome to Muse!!"
+
+
+
+# ***** Old stuff below...
+
+# from scikits.audiolab import *
 # from scikits.samplerate import *
 # import scikits.samplerate
-from scikits.samplerate import resample as src
+# from scikits.samplerate import resample as src
 
+# from sndfiles import *        # the old soundfiles module to be deprecated
+#from sndfile import *        # the new PSL aifc/wave interface
 
-# now import the muse modules into muse namespace
-from sndfiles import *
-from muse import *
-# from generators import *
-from filters import *
-from decoders import *
-from encoders import *
-from transforms import *
-from generators import *
-from nonlinear import *
-from analysis import *
-
-# import muse
-# import filters
-# import decoders
-# import encoders
-# import transforms
-
-print "Welcome to Muse!!"
+#from muse import *
+#from filters    import *
+#from decoders   import *
+#from encoders   import *
+#from transforms import *
+#from generators import *
+#from nonlinear  import *
+#from analysis   import *
