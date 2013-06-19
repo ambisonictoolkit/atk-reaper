33d66d0f685197210de8cf257b57023bd570d2ba
Trond Lossius <trond.lossius@bek.no>
2013-06-19 13:44:29 +0200

    Minor change to script in order for plugins to show up earlier in the list of JS plugs in Reaper

--- tools/copyPluginsToReaper.rb
+++ tools/copyPluginsToReaper.rb
@@ -26,7 +26,7 @@ def copyPluginsToReaper
   if mac?
     
     # Create plugin folder if it doesn't exist already
-    pluginFolderPath = File.expand_path("~") + "/Library/Application Support/REAPER/Effects/ambi-toolkit"
+    pluginFolderPath = File.expand_path("~") + "/Library/Application Support/REAPER/Effects/AmbisonicToolkit"
     FileUtils.mkdir_p(pluginFolderPath) unless File.exists?(pluginFolderPath)
     
     # Copy plugins
